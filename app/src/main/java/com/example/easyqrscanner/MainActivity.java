524c91f4e6df74dc1473b945816cdb1d0d645362
frinno_codes <dev.frinno@gmail.com>
2020-01-17 02:06:40 +0300

    Easy QR Scanner With working Bottom Navigation

---
+++ app/src/main/java/com/example/easyqrscanner/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.easyqrscanner;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNav = findViewById(R.id.nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //Scanner Fragment As root.
+        getSupportFragmentManager().beginTransaction().replace(R.id.frag_container,
+                new ScannedFragment()).commit();
+
+
+        /*FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment  = null;
+                    switch (item.getItemId()){
+                        case R.id.nav_scanned:
+                            selectedFragment = new ScannedFragment();
+                            break;
+                        case R.id.nav_empty:
+                            selectedFragment = new ScannerFragment();
+                            break;
+                        case R.id.nav_settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frag_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+
+}
